4bba67a2b25f3a3976bd50e41248f02b8b2eedda
jreyesr <jreyesr@users.noreply.github.com>
2021-08-27 11:18:26 -0500

    BUGFIX: Center Add List card when on second column

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,12 +4,19 @@
       <div class="col-12 col-md-6" v-for="item in items" :key="item.name">
         <list-of-ideas-card :item="item" />
       </div>
-      <div class="col-12 col-md-6">
-        <new-list-card/>
+      <div class="col-12 col-md-6 center-children-y">
+        <new-list-card style="flex-grow: 1;"/>
       </div>
     </div>
   </q-page>
 </template>
+
+<style lang="scss">
+.center-children-y {
+  display: flex;
+  align-items: center;
+}
+</style>
 
 <script>
 import { defineComponent, computed } from 'vue'
